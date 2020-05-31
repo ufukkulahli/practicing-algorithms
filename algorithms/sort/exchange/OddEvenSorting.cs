a8bd615d293f0af047b4d061b2a3367ed28659a2
ufukkulahli <ufukkulahli@gmail.com>
2020-05-31 13:12:47 +0300

    Refactoring 'OddEvenSorting'.

--- algorithms/sort/exchange/OddEvenSorting.cs
+++ algorithms/sort/exchange/OddEvenSorting.cs
@@ -12,14 +12,15 @@ namespace practicing_algorithms.algorithms.sort.exchange
       while(notSortedYet)
       {
         notSortedYet = false;
+        var sn = new SortedNumbers(numbers);
 
         var oddIndexes = new OddNumbers().GenerateUpTo(numbers.Length-2);
-        var oddIndexSorting = new SortedNumbers(numbers, oddIndexes);
-        notSortedYet = oddIndexSorting.Sort();
+        var oddIndexSorting = sn;
+        notSortedYet = oddIndexSorting.ByIndexes(oddIndexes);
 
         var evenIndexes = new EvenNumbers().GenerateUpTo(numbers.Length-2);
-        var evenIndexSorting = new SortedNumbers(numbers, evenIndexes);
-        notSortedYet = evenIndexSorting.Sort();
+        var evenIndexSorting = sn;
+        notSortedYet = evenIndexSorting.ByIndexes(evenIndexes);
       }
     }
   }
