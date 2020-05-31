bd08c84c2a6a77b2faf25fa7d10a85e83d6e8f19
ufukkulahli <ufukkulahli@gmail.com>
2020-05-31 13:43:58 +0300

    Refactoring 'OddEvenSorting'.

--- algorithms/sort/exchange/Numbers.cs
+++ algorithms/sort/exchange/Numbers.cs
@@ -5,7 +5,6 @@ namespace practicing_algorithms.algorithms.sort.exchange
   public sealed class Numbers
   {
     readonly int[] numbers;
-    readonly IEnumerable<int> indexes;
     bool notSortedYet;
 
     public Numbers(int[] numbers) => this.numbers = numbers;
@@ -20,11 +19,19 @@ namespace practicing_algorithms.algorithms.sort.exchange
         if(currentLeftNumber > currentRightNumber)
         {
           notSortedYet     = true;
-          numbers[index]   = currentRightNumber;
-          numbers[index+1] = currentLeftNumber;
+          SwapNumberAndNextAt(index);
         }
       }
       return notSortedYet;
+    }
+
+    public void SwapNumberAndNextAt(int index)
+    {
+      var leftNumber   = numbers[index];
+      var rightNumber  = numbers[index+1];
+
+      numbers[index]   = rightNumber;
+      numbers[index+1] = leftNumber;
     }
   }
 }
