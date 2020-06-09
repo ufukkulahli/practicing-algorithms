f4ee53b2fabaf10c17f1dff333a260abc6f7b3c3
Ufuk Kulahli <ufukkulahli@gmail.com>
2020-06-09 23:01:54 +0300

    Continue Heap sort.

--- algorithms/sort/selection/HeapSorting.cs
+++ algorithms/sort/selection/HeapSorting.cs
@@ -5,5 +5,29 @@ namespace practicing_algorithms.algorithms.sort.selection
   public sealed class HeapSorting
   {
     public void Sort() => throw new NotImplementedException();
+    
+    void Heapify(int[] array, int length, int i)
+    {
+        var largest = i;
+        var left    = 2 * i + 1;
+        var right   = 2 * i + 2;
+      
+        if (left < length && array[left] > array[largest])
+        {
+            largest = left;
+        }
+        if (right < length && array[right] > array[largest])
+        {
+            largest = right;
+        }
+        if (largest != i)
+        {
+            var swap       = array[i];
+            array[i]       = array[largest];
+            array[largest] = swap;
+          
+            Heapify(array, length, largest);
+        }
+    }
   }
 }
