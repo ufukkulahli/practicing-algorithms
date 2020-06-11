35647df2e8b0b195228a45f11d0d99ec55a06e29
Ufuk Kulahli <ufukkulahli@gmail.com>
2020-06-11 16:47:26 +0300

    Continue ‘HeapSorting’.

--- algorithms/sort/selection/HeapSorting.cs
+++ algorithms/sort/selection/HeapSorting.cs
@@ -5,13 +5,33 @@ namespace practicing_algorithms.algorithms.sort.selection
   public sealed class HeapSorting
   {
     public void Sort() => throw new NotImplementedException();
-    
+
+    void Sort(int[] array)
+    {
+      for(var index=FindStartingIndex(array); index>=0; index--)
+      {
+        RebuildHeap(array, array.Length, index);
+      }
+
+      for(var index=(array.Length-1); index>=0; index--)
+      {
+        var firstItem = array[0];
+
+        array[0]     = array[index];
+        array[index] = firstItem;
+
+        RebuildHeap(array, index, 0);
+      }
+    }
+
+    int FindStartingIndex(int[] array) => (array.Length/2) - 1;
+
     void RebuildHeap(int[] array, int length, int i)
     {
         var largest = i;
         var left    = 2 * i + 1;
         var right   = 2 * i + 2;
-      
+
         if (left < length && array[left] > array[largest])
         {
             largest = left;
@@ -25,7 +45,7 @@ namespace practicing_algorithms.algorithms.sort.selection
             var swap       = array[i];
             array[i]       = array[largest];
             array[largest] = swap;
-          
+
             RebuildHeap(array, length, largest);
         }
     }
