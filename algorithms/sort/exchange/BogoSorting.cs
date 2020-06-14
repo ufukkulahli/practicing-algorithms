3a13919aa1fb5275200275b5ecd50cf4420da651
Ufuk Kulahli <ufukkulahli@gmail.com>
2020-06-14 22:12:44 +0300

    Start ‘Bogo’ sorting algorithm.

---
+++ algorithms/sort/exchange/BogoSorting.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace practicing_algorithms.algorithms.sort.exchange
+{
+  public sealed class BogoSorting
+  {
+    public void Sort() => throw new NotImplementedException();
+  }
+}
