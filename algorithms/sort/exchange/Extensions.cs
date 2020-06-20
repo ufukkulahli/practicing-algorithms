773c23f3d4d786e506f59575fd688a86f0d31eca
ufukkulahli <ufukkulahli@gmail.com>
2020-06-20 23:38:10 +0300

    Delete redundant class.

--- algorithms/sort/exchange/Extensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace practicing_algorithms.algorithms.sort.exchange
-{
-  public static class Extensions
-  {
-    public static void WriteLine<T>(this T value) => Console.WriteLine(value);
-  }
-}
