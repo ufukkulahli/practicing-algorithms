3de0f7c5f1afd6a9e7f4ac5e8b043f6ee3ebaa18
ufukkulahli <ufukkulahli@gmail.com>
2020-06-21 22:56:41 +0300

    Fix failing test because of previous 'method param deleting'.

--- algorithms/sort/selection/HeapSorting.cs
+++ algorithms/sort/selection/HeapSorting.cs
@@ -3,54 +3,49 @@ namespace practicing_algorithms.algorithms.sort.selection
   public sealed class HeapSorting
   {
     readonly int[] unorderedNumbers;
+    readonly Numbers numbers;
 
     public HeapSorting(int[] unorderedNumbers)
     {
       this.unorderedNumbers = unorderedNumbers;
+      this.numbers          = new Numbers(unorderedNumbers);
     }
 
     public void Sort()
     {
       for(var index=FindStartingIndex(unorderedNumbers); index>=0; index--)
       {
-        RebuildHeap(index);
+        RebuildHeap(unorderedNumbers.Length, index);
       }
 
       for(var index=(unorderedNumbers.Count()); index>=0; index--)
       {
-        var firstItem = unorderedNumbers[0];
-
-        unorderedNumbers[0]     = unorderedNumbers[index];
-        unorderedNumbers[index] = firstItem;
-
-        RebuildHeap(0);
+        numbers.SwapNumbersAtIndexes(0, index);
+        RebuildHeap(index, 0);
       }
     }
 
     int FindStartingIndex(int[] unorderedNumbers) => (unorderedNumbers.Length/2) - 1;
 
-    void RebuildHeap(int i)
+    void RebuildHeap(int length, int i)
     {
-        var largest = i;
-        var left    = 2 * i + 1;
-        var right   = 2 * i + 2;
+      var largest = i;
+      var left    = 2 * i + 1;
+      var right   = 2 * i + 2;
 
-        if (left < unorderedNumbers.Length && unorderedNumbers[left] > unorderedNumbers[largest])
-        {
-            largest = left;
-        }
-        if (right < unorderedNumbers.Length && unorderedNumbers[right] > unorderedNumbers[largest])
-        {
-            largest = right;
-        }
-        if (largest != i)
-        {
-            var numberToBeSwapped     = unorderedNumbers[i];
-            unorderedNumbers[i]       = unorderedNumbers[largest];
-            unorderedNumbers[largest] = numberToBeSwapped;
-
-            RebuildHeap(largest);
-        }
+      if (left < length && unorderedNumbers[left] > unorderedNumbers[largest])
+      {
+        largest = left;
+      }
+      if (right < length && unorderedNumbers[right] > unorderedNumbers[largest])
+      {
+        largest = right;
+      }
+      if (largest != i)
+      {
+        numbers.SwapNumbersAtIndexes(i, largest);
+        RebuildHeap(length, largest);
+      }
     }
   }
 }
