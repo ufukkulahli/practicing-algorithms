73be958d5b28abe8aa978baba2d81bca2fd250fb
ufukkulahli <ufukkulahli@gmail.com>
2020-06-23 23:38:25 +0300

    Continue 'Selection' sorting algorithm.

--- algorithms/sort/selection/SelectionSorting.cs
+++ algorithms/sort/selection/SelectionSorting.cs
@@ -4,6 +4,10 @@ namespace practicing_algorithms.algorithms.sort.selection
 {
   public sealed class SelectionSorting
   {
+    readonly int[] unorderedNumbers;
+
+    public SelectionSorting(int[] unorderedNumbers) => this.unorderedNumbers = unorderedNumbers;
+
     public void Sort() => throw new NotImplementedException();
   }
 }
--- tests/sort/selection/SelectionSorting.cs
+++ tests/sort/selection/SelectionSorting.cs
@@ -9,8 +9,13 @@ namespace practicing_algorithms.tests.sort.selection
     [Fact]
     public void Test()
     {
-      // Arrange && Act && Assert
-      Assert.Throws<NotImplementedException>( () => new SelectionSorting().Sort() );
+      // Arrange
+      var numbers       = new int[5]{5,4,3,2,1};
+      var sortedNumbers = new int[5]{1,2,3,4,5};
+
+      // Act && Assert
+      Assert.Throws<NotImplementedException>( () => new SelectionSorting(numbers).Sort() );
+      Assert.NotEqual(sortedNumbers, numbers);
     }
   }
 }
