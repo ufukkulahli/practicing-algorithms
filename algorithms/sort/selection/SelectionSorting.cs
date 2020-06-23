9463862d9a39033440590b6429837c49b69000e3
ufukkulahli <ufukkulahli@gmail.com>
2020-06-23 23:31:41 +0300

    Start 'Selection' sorting algorithm.

---
+++ algorithms/sort/selection/SelectionSorting.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace practicing_algorithms.algorithms.sort.selection
+{
+  public sealed class SelectionSorting
+  {
+    public void Sort() => throw new NotImplementedException();
+  }
+}
---
+++ tests/sort/selection/SelectionSorting.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using System;
+using practicing_algorithms.algorithms.sort.selection;
+
+namespace practicing_algorithms.tests.sort.selection
+{
+  public class SelectionSortingTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // Arrange && Act && Assert
+      Assert.Throws<NotImplementedException>( () => new SelectionSorting().Sort() );
+    }
+  }
+}
