bf814e01c8f60f41c876dc8b5a3472e49733d8b1
ufukkulahli <ufukkulahli@gmail.com>
2020-07-01 23:07:59 +0300

    Continue 'Gnome' sorting algorithm.

--- algorithms/sort/exchange/GnomeSorting.cs
+++ algorithms/sort/exchange/GnomeSorting.cs
@@ -3,15 +3,42 @@ namespace practicing_algorithms.algorithms.sort.exchange
   public sealed class GnomeSorting
   {
     readonly int[] unorderedNumbers;
+    readonly Numbers numbers;
+    int gnomesPosition;
 
     public GnomeSorting(int[] unorderedNumbers)
     {
       this.unorderedNumbers = unorderedNumbers;
+      this.numbers = new Numbers(unorderedNumbers);
     }
 
     public void Sort()
     {
-      throw new System.NotImplementedException();
+      while (GnomeCouldStillAdvance())
+      {
+        if(GnomeIsAtTheStartPosition())
+        {
+          AdvanceGnomesPosition();
+          continue;
+        }
+
+        var leftNumber = unorderedNumbers[gnomesPosition-1];
+        var rightNumber= unorderedNumbers[gnomesPosition];
+
+        if(rightNumber >= leftNumber)
+        {
+          AdvanceGnomesPosition();
+          continue;
+        }
+
+        numbers.SwapNumbersAtIndexes(gnomesPosition, gnomesPosition-1);
+        DeclineGnomesPosition();
+      }
     }
+
+    public bool GnomeCouldStillAdvance()    => gnomesPosition < unorderedNumbers.Length;
+    public bool GnomeIsAtTheStartPosition() => gnomesPosition == 0;
+    public void AdvanceGnomesPosition()     => gnomesPosition ++;
+    public void DeclineGnomesPosition()     => gnomesPosition --;
   }
 }
