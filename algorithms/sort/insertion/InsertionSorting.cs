6a406a411031eb32c5e7a0e65fea892e82803f44
ufukkulahli <ufukkulahli@gmail.com>
2020-07-02 22:25:30 +0300

    Start 'Insertion' sorting algorithm.

---
+++ algorithms/sort/insertion/InsertionSorting.cs
@@ -0,0 +1,17 @@
+namespace practicing_algorithms.algorithms.sort.insertion
+{
+  public sealed class InsertionSorting
+  {
+    readonly int[] unorderedNumbers;
+
+    public InsertionSorting(int[] unorderedNumbers)
+    {
+      this.unorderedNumbers = unorderedNumbers;
+    }
+
+    public void Sort()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
