3a4f40fcc9331579f0108706b3999eccf53333fb
ufukkulahli <ufukkulahli@gmail.com>
2020-07-06 23:30:14 +0300

    Continue 'Shell' sorting algorithm.

--- algorithms/sort/insertion/ShellSorting.cs
+++ algorithms/sort/insertion/ShellSorting.cs
@@ -13,5 +13,24 @@ namespace practicing_algorithms.algorithms.sort.insertion
     {
       throw new System.NotImplementedException();
     }
+
+    public void Sort(int[] array)
+    {
+      for (var gapSize=(array.Length/2);  gapSize>0;  gapSize/= 2)
+      {
+        for (var index=gapSize;  index<array.Length;  index+= 1)
+        {
+          var currentNumber = array[index];
+          var j=0;
+
+          for (j = index;  (j>=gapSize && array[j-gapSize] > currentNumber);  j-= gapSize)
+          {
+            array[j] = array[j-gapSize];
+          }
+
+          array[j] = currentNumber;
+        }
+      }
+    }
   }
 }
