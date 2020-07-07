74b2c576ce0f35050b6fb067c448c35926b9fd81
ufukkulahli <ufukkulahli@gmail.com>
2020-07-07 22:21:49 +0300

    Continue 'Shell' sorting.

--- algorithms/sort/insertion/ShellSorting.cs
+++ algorithms/sort/insertion/ShellSorting.cs
@@ -11,24 +11,19 @@ namespace practicing_algorithms.algorithms.sort.insertion
 
     public void Sort()
     {
-      throw new System.NotImplementedException();
-    }
-
-    public void Sort(int[] array)
-    {
-      for (var gapSize=(array.Length/2);  gapSize>0;  gapSize/= 2)
+      for (var gapSize=(unorderedNumbers.Length/2);  gapSize>0;  gapSize/= 2)
       {
-        for (var index=gapSize;  index<array.Length;  index+= 1)
+        for (var index=gapSize;  index<unorderedNumbers.Length;  index+= 1)
         {
-          var currentNumber = array[index];
+          var currentNumber = unorderedNumbers[index];
           var j=0;
 
-          for (j = index;  (j>=gapSize && array[j-gapSize] > currentNumber);  j-= gapSize)
+          for (j = index;  (j>=gapSize && unorderedNumbers[j-gapSize] > currentNumber);  j-= gapSize)
           {
-            array[j] = array[j-gapSize];
+            unorderedNumbers[j] = unorderedNumbers[j-gapSize];
           }
 
-          array[j] = currentNumber;
+          unorderedNumbers[j] = currentNumber;
         }
       }
     }
