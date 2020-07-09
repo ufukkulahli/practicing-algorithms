de45b36d1cc0d5bd469c508f1d4cabef2a6aab91
ufukkulahli <ufukkulahli@gmail.com>
2020-07-09 21:23:31 +0300

    Start 'Comb' sorting.

---
+++ algorithms/sort/exchange/CombSorting.cs
@@ -0,0 +1,17 @@
+namespace practicing_algorithms.algorithms.sort.exchange
+{
+  public sealed class CombSorting
+  {
+    readonly int[] unorderedNumbers;
+
+    public CombSorting(int[] unorderedNumbers)
+    {
+      this.unorderedNumbers = unorderedNumbers;
+    }
+
+    public void Sort()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
