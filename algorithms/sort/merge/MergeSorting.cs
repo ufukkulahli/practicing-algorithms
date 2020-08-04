f8d69337cd3a3dabb1727fe8379c3749b9232d54
ufukkulahli <ufukkulahli@gmail.com>
2020-08-04 21:35:50 +0300

    Start 'Merge' sorting algorithm.

---
+++ algorithms/sort/merge/MergeSorting.cs
@@ -0,0 +1,10 @@
+namespace practicing_algorithms.algorithms.sort.merge
+{
+  public sealed class MergeSorting
+  {
+    public void Sort()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
---
+++ tests/sort/merge/MergeSortingTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using practicing_algorithms.algorithms.sort.merge;
+
+namespace practicing_algorithms.tests.sort.merge
+{
+  public class MergeSortingTests
+  {
+    [Fact]
+    public void Test()
+    {
+      Assert.Throws<System.NotImplementedException>( () => new MergeSorting().Sort() );
+    }
+  }
+}
