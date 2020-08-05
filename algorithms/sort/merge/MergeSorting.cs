f8d7aedf74b3b83ce64b5297313246df2a4ab71f
ufukkulahli <ufukkulahli@gmail.com>
2020-08-05 23:01:49 +0300

    Continue 'Merge' sorting.

--- algorithms/sort/merge/MergeSorting.cs
+++ algorithms/sort/merge/MergeSorting.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace practicing_algorithms.algorithms.sort.merge
 {
   public sealed class MergeSorting
@@ -5,6 +8,48 @@ namespace practicing_algorithms.algorithms.sort.merge
     public void Sort()
     {
       throw new System.NotImplementedException();
+    }
+
+    public IEnumerable<int> Merge(List<int> left, List<int> right)
+    {
+      List<int> result = new List<int>();
+
+      var leftAny = left.Any();
+      var rightAny = right.Any();
+      int firstOfLeft = left.First();
+      var firstOfRight = right.First();
+
+      while (leftAny || rightAny)
+      {
+        if (leftAny && rightAny)
+        {
+          if (firstOfLeft <= firstOfRight)
+          {
+            result.Add(firstOfLeft);
+            left.Remove(firstOfLeft);
+          }
+          else
+          {
+            result.Add(firstOfRight);
+            right.Remove(firstOfRight);
+          }
+          continue;
+        }
+
+        if (leftAny)
+        {
+          result.Add(firstOfLeft);
+          left.Remove(firstOfLeft);
+          continue;
+        }
+
+        if (rightAny)
+        {
+          result.Add(firstOfRight);
+          right.Remove(firstOfRight);
+        }
+      }
+      return result;
     }
   }
 }
