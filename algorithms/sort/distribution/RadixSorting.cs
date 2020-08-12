fa716ab70358844add75c4234fee43d496ae3494
Ufuk Kulahli <ufukkulahli@gmail.com>
2020-08-12 21:26:44 +0300

    Continue ‘Radix’ sorting, add ‘FindBiggest’ method.

--- algorithms/sort/distribution/RadixSorting.cs
+++ algorithms/sort/distribution/RadixSorting.cs
@@ -6,5 +6,19 @@ namespace practicing_algorithms.algorithms.sort.distribution
     {
       throw new System.NotImplementedException();
     }
+    
+    public int FindBiggest(int[] numbers)  
+    {  
+        var biggest = arr[0];  
+        for (var index = 1; index < numbers.Length; index++)
+        {
+          if (numbers[index] > biggest)
+          {
+            biggest = numbers[index];
+          }
+        }
+              
+        return biggest;  
+    }
   }
 }
