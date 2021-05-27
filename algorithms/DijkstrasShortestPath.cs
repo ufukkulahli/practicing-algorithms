bb2a131c2ec3e3e507869d6dbf25f78747a61b62
ufukkulahli <ufukkulahli@gmail.com>
2021-05-27 22:49:24 +0300

    Continue 'Dijkstra's shortest path' algorithm, add functionality, test.

--- algorithms/DijkstrasShortestPath.cs
+++ algorithms/DijkstrasShortestPath.cs
@@ -2,9 +2,22 @@ namespace practicing_algorithms.algorithms
 {
   public class DijkstrasShortestPath
   {
+    public int [] shortestDistances {get;} = new int[9];
+    public bool[] visitedVertices   {get;} = new bool[9];
+
     public void Find(int[,] graph, int source)
     {
       throw new System.NotImplementedException();
     }
+
+    public void Reset()
+    {
+      for(var i=0; i<9; i++)
+      {
+        this.shortestDistances[i] = int.MaxValue;
+        this.visitedVertices  [i] = false;
+      }
+    }
+
   }
 }
--- tests/DijkstrasShortestPathTests.cs
+++ tests/DijkstrasShortestPathTests.cs
@@ -10,5 +10,37 @@ namespace practicing_algorithms.tests
     {
       Assert.Throws<System.NotImplementedException>( () => new DijkstrasShortestPath().Find(null, 0) );
     }
+
+    [Fact]
+    public void ResetAllDistancesToInfinite_And_ResetVisitedVertices()
+    {
+      // Arrange
+      var shortestPath = new DijkstrasShortestPath();
+
+      // Act
+      shortestPath.Reset();
+
+      // Assert
+      Assert.Equal(2147483647, shortestPath.shortestDistances[0]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[1]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[2]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[3]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[4]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[5]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[6]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[7]);
+      Assert.Equal(2147483647, shortestPath.shortestDistances[8]);
+
+      Assert.False(shortestPath.visitedVertices[0]);
+      Assert.False(shortestPath.visitedVertices[1]);
+      Assert.False(shortestPath.visitedVertices[2]);
+      Assert.False(shortestPath.visitedVertices[3]);
+      Assert.False(shortestPath.visitedVertices[4]);
+      Assert.False(shortestPath.visitedVertices[5]);
+      Assert.False(shortestPath.visitedVertices[6]);
+      Assert.False(shortestPath.visitedVertices[7]);
+      Assert.False(shortestPath.visitedVertices[8]);
+    }
+
   }
 }
