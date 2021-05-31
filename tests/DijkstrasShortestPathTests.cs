dcbbdb06f362da89f277610f92e07267f9616280
ufukkulahli <ufukkulahli@gmail.com>
2021-05-31 22:11:40 +0300

    Continue 'Dijkstra's shortest path' algorithm, add test.

--- tests/DijkstrasShortestPathTests.cs
+++ tests/DijkstrasShortestPathTests.cs
@@ -102,5 +102,65 @@ namespace practicing_algorithms.tests
       Assert.Equal(2147483647 , shortestPath.shortestDistances[8]);
     }
 
+    [Fact]
+    public void DoesNotUpdatePath()
+    {
+      // Arrange
+      var shortestPath = new DijkstrasShortestPath();
+      shortestPath.Reset();
+
+      var graph = new int[,]
+      {
+        { 0, 0, 0 },
+        { 3, 1, 0 },
+        { 0, 7, 0 }
+      };
+
+      var index_u = 1;
+      var index_v = 0;
+
+      // Pre-act 1
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[0]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[1]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[2]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[3]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[4]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[5]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[6]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[7]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[8]);
+
+      // Manipulate data for testing purposes.
+      // These values will be set in the loop
+      shortestPath.ResetDistanceOfSourceVertex(sourceVertex: index_u);
+      shortestPath.SetShortestDistance(index_u, value: 2);
+      shortestPath.SetShortestDistance(index_v, value: 4); // v=4 is the reason to not to update
+
+      // Pre-act 2
+      Assert.Equal(4          , shortestPath.shortestDistances[0]);
+      Assert.Equal(2          , shortestPath.shortestDistances[1]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[2]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[3]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[4]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[5]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[6]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[7]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[8]);
+
+      // Act
+      shortestPath.Update(graph, index_u, index_v);
+
+      // Assert
+      Assert.Equal(4          , shortestPath.shortestDistances[0]);
+      Assert.Equal(2          , shortestPath.shortestDistances[1]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[2]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[3]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[4]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[5]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[6]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[7]);
+      Assert.Equal(2147483647 , shortestPath.shortestDistances[8]);
+    }
+
   }
 }
